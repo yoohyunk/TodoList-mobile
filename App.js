c15ffb840306a12ebef01d834375f7923eba07d0
yoohyunk <yoohyunk20@gmail.com>
2025-02-10 08:37:05 -0700

    first commit -  base code

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text
+        style={{
+          fontSize: 20,
+          borderBottomWidth: 2,
+          borderBottomColor: "black",
+          width: "100%",
+          padding: 10,
+        }}
+      >
+        Todo List App
+      </Text>
+      /
+      <ScrollView style={{ width: "100%" }}>
+        <View style={styles.task}>
+          <Text>Task 1: Buy groceries</Text>
+        </View>
+        <View style={styles.task}>
+          <Text>Task 2: Walk the dog</Text>
+        </View>
+        <View style={styles.task}>
+          <Text>Task 3: Read a book</Text>
+        </View>
+        <View
+          style={styles.formContainer}
+          automaticallyAdjustKeyboardInsets={true}
+        >
+          <TextInput style={styles.input} placeholder="Add a new task" />
+          <Button title="Add" onPress={() => {}} />
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+    width: "100%",
+  },
+  task: {
+    padding: 10,
+    marginBottom: 5,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  formContainer: {
+    paddingBottom: 50,
+    flexDirection: "row",
+    padding: 20,
+  },
+  input: {
+    flex: 1,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginRight: 10,
   },
 });
